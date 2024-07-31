d1a3fd2417c298f066d80f228a688d47008280a0
aesthetic-what <ladnoladno477@gmail.com>
2024-07-31 09:50:23 +0500

    module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -8,8 +8,10 @@ class Animal:
     def eat(self, food):
         if food.edible == True:
             print(f"{self.name} съел {food.name}")
+            self.fed = True
         else:
             print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 
 
 class Plant:
